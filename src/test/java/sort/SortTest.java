c399e51d986550a439c9dfa1742576aa7db3cf5d
Brian <bwook9908@gmail.com>
2023-08-14 10:13:13 +0900

    init

--- src/test/java/sort/SortTest.java
+++ src/test/java/sort/SortTest.java
@@ -1,5 +1,7 @@
-package algorithms.sort;
+package sort;
 
+import algorithms.sort.BubbleSort;
+import algorithms.sort.SelectionSort;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
